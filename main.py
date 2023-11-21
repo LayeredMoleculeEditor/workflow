d13084c7bf60f8eb39906b39018f8ed7de6b9691
Hexiang Qi <qihexiang@outlook.com>
2023-11-21 20:11:58 +0800

    try catch example

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from python_sdk import s
+from python_sdk import config, Workspace
 
-print(s.get("http://localhost:18010/"))
+config["prefix"] = "http://localhost:18010"
+
+try:
+    ws = Workspace.create("example")
+    print(ws)
+    print(ws.clone_stack(0))
+    print(ws.get_stacks())
+    print(ws.remove())
+except:
+    ws.remove()
