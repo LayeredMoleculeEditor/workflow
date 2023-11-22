7f6f8faba9f5159ad925d1336d8e5bfb62ebc8c8
Hexiang Qi <qihexiang@outlook.com>
2023-11-22 16:29:57 +0800

    aiohttp

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from python_sdk import config, Workspace
+from python_sdk import Workspace
 
-config["prefix"] = "http://localhost:18010"
+async def main():
+    ws = Workspace("http://localhost:18010", "example")
+    try:
+        await ws.create()
+        print(ws)
+        print(await ws.get_stacks())
+        print(await ws.clone_stack(0))
+        print(await ws.get_stack(1))
+        print(await ws.is_stack_writable(1))
+        print(await ws.overlay_fill_layer(1))
+        print(await ws.is_stack_writable(1))
+        print(await ws.write_to_layer(1, {
+            0: {
+                "element": 6, "position": [0.,0.,0.] 
+            },
+            1: {
+                "element": 1, "position": [1., 0., 0.]
+            },
+            2: {
+                "element":15, "position": [0., 0., -1.]
+            },
+        }, {
+            "indexes": [(0, 1), (0, 2)],
+            "values": [1, 3]
+        }))
+        ws.overlay_fill_layer(1)
+        print(await ws.write_to_layer(1, {
+            1: None,
+            2: {
+                "element": 5, "position": [0., 0., -1.]
+            }
+        }, {}))
+        print(await ws.get_stack(1))
+    finally:
+        print(f"Remove created Workspaces: {await ws.remove()}")
 
-try:
-    ws = Workspace.create("example")
-    print(ws)
-    print(ws.clone_stack(0))
-    print(ws.get_stacks())
-    print(ws.remove())
-except:
-    ws.remove()
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
