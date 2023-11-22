9cfa247eb8e8ddad695ec23f25b3109d3aa26126
Hexiang Qi <qihexiang@outlook.com>
2023-11-22 18:37:58 +0800

    await for fill layer

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ async def main():
     ws = Workspace("http://localhost:18010", "example")
     try:
         await ws.create()
-        print(ws)
         print(await ws.get_stacks())
         print(await ws.clone_stack(0))
         print(await ws.get_stack(1))
@@ -25,7 +24,7 @@ async def main():
             "indexes": [(0, 1), (0, 2)],
             "values": [1, 3]
         }))
-        ws.overlay_fill_layer(1)
+        await ws.overlay_fill_layer(1)
         print(await ws.write_to_layer(1, {
             1: None,
             2: {
