ae2dd89bb4f4e5ee46c0d8b4aec8e889f500fe1c
Hexiang Qi <qihexiang@outlook.com>
2023-11-24 11:47:54 +0800

    add substitute example

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from python_sdk import Workspace
+from python_sdk import AddSubstitute, Workspace
 
 async def main():
     ws = Workspace("http://localhost:18010", "example")
     try:
         await ws.create()
-        print(await ws.get_stacks())
-        print(await ws.clone_stack(0))
-        print(await ws.get_stack(1))
-        print(await ws.is_stack_writable(1))
-        print(await ws.overlay_fill_layer(1))
-        print(await ws.is_stack_writable(1))
-        print(await ws.write_to_layer(1, {
+        await ws.get_stacks()
+        await ws.clone_stack(0)
+        await ws.get_stack(1)
+        await ws.is_stack_writable(1)
+        await ws.overlay_fill_layer(1)
+        await ws.is_stack_writable(1)
+        await ws.write_to_layer(1, {
             0: {
                 "element": 6, "position": [0.,0.,0.] 
             },
@@ -23,16 +23,37 @@ async def main():
         }, {
             "indexes": [(0, 1), (0, 2)],
             "values": [1, 3]
-        }))
+        })
         await ws.overlay_fill_layer(1)
-        print(await ws.write_to_layer(1, {
-            1: None,
-            2: {
-                "element": 5, "position": [0., 0., -1.]
-            }
-        }, {}))
-        print(await ws.get_stack(1))
+        add_substituent: AddSubstitute = {
+            "atoms": [
+                {
+                    "element": 1,
+                    "position": [0., 0., 0.]
+                },
+                {
+                    "element": 6,
+                    "position": [0., 1., 0.]
+                },
+                {
+                    "element": 5,
+                    "position": [0., 2., 0.]
+                }
+            ],
+            "bonds_idxs": [
+                (0,1), (1,2)
+            ],
+            "bonds_values": [
+                1.0, 3.0
+            ],
+            "class_name": "CN",
+            "current": (0, 1),
+            "target": (0, 1),
+        }
+        await ws.add_substitute(1, add_substituent)
+        print(await ws.cleaned_molecule(1))
     finally:
+        # pass
         print(f"Remove created Workspaces: {await ws.remove()}")
 
 if __name__ == "__main__":
