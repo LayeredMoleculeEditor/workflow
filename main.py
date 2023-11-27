3d4415ce12cb052e3014300ece6ce8afba0853a5
Hexiang Qi <qihexiang@outlook.com>
2023-11-27 03:08:37 +0000

    Intergrate with ASE

--- main.py
+++ main.py
@@ -1,60 +1,32 @@
 from python_sdk import AddSubstitute, Workspace
+from ase.io.gaussian import read_gaussian_in
+from ase import Atom, Atoms
 
 async def main():
     ws = Workspace("http://localhost:18010", "example")
+    template = read_gaussian_in(open("./template.gjf"))
+    atoms = [{"element": int(atom.number), "position": list(atom.position)} for atom in template]
     try:
         await ws.create()
-        await ws.get_stacks()
-        await ws.clone_stack(0)
-        await ws.get_stack(1)
-        await ws.is_stack_writable(1)
-        await ws.overlay_fill_layer([1])
-        await ws.is_stack_writable(1)
-        await ws.write_to_layer(1, {
-            0: {
-                "element": 6, "position": [0.,0.,0.] 
-            },
-            1: {
-                "element": 1, "position": [1., 0., 0.]
-            },
-            2: {
-                "element":15, "position": [0., 0., -1.]
-            },
-        }, {
-            "indexes": [(0, 1), (0, 2)],
-            "values": [1, 3]
-        })
-        await ws.overlay_fill_layer([1])
-        add_substituent: AddSubstitute = {
-            "atoms": [
-                {
-                    "element": 1,
-                    "position": [0., 0., 0.]
-                },
-                {
-                    "element": 6,
-                    "position": [0., 1., 0.]
-                },
-                {
-                    "element": 5,
-                    "position": [0., 2., 0.]
-                }
-            ],
-            "bonds_idxs": [
-                (0,1), (1,2)
-            ],
-            "bonds_values": [
-                1.0, 3.0
-            ],
-            "class_name": "CN",
-            "current": (0, 1),
-            "target": (0, 1),
+        await ws.overlay_fill_layer([0])
+        await ws.import_structure(0, (atoms, [], []), "base")
+        benzene = [{"element": int(atom.number), "position": list(atom.position)} for atom in read_gaussian_in(open("./benzene.gjf"))]
+        add_benzene: AddSubstitute = {
+            "atoms": benzene,
+            "bonds_idxs": [],
+            "bonds_values": [],
+            "class_name": "left",
+            "current": (7, 1),
+            "target": (12, 19)
         }
-        await ws.add_substitute(1, add_substituent)
-        print(await ws.cleaned_molecule(1))
+        await ws.clone_stack(0)
+        await ws.add_substitute(1, add_benzene)
+        [atoms, _, _] = await ws.cleaned_molecule(1)
+        atoms = map(lambda atom: Atom(atom["element"], atom["position"]), atoms)
     finally:
         # pass
         print(f"Remove created Workspaces: {await ws.remove()}")
+        await ws.close()
 
 if __name__ == "__main__":
     import asyncio
