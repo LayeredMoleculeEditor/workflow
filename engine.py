898dea5fc9e5866a26d4ba8743b49902e7caa1d8
Hexiang Qi <qihexiang@outlook.com>
2023-12-10 01:55:26 +0000

    fix bug of load os.environ

--- engine.py
+++ engine.py
@@ -1,27 +1,33 @@
-from abc import ABC, abstractclassmethod, abstractproperty
 import asyncio
 import json
 import os
 import os.path
 import pathlib
-from typing import TypeVar, TypedDict
+from typing import Any
 
 from pydantic import BaseModel
 
 from python_sdk import Workspace
 
-LME_CACHE_DIR = pathlib.Path.home() if os.environ["LME_CACHE_DIR"] is None else os.environ["LME_CACHE_DIR"]
+LME_CACHE_DIR = (
+    pathlib.Path.home()
+    if os.environ.get("LME_CACHE_DIR") is None
+    else os.environ["LME_CACHE_DIR"]
+)
+
 
 class RunnerContext(BaseModel):
     start: int
     tags: list[dict[str, str]]
 
+
 class Metas(BaseModel):
     name: str
     envs: dict[str, str]
-    
+
+
 class RunnerBase:
-    def __init__(self, metas: Metas, options) -> None:
+    def __init__(self, metas: Metas, options: Any) -> None:
         self.__metas__ = metas
 
     def cache(self) -> int:
@@ -30,18 +36,19 @@ class RunnerBase:
     async def init_resources(self) -> None:
         pass
 
-    async def execute(self, workspace: Workspace, context: RunnerContext) -> RunnerContext:
+    async def execute(
+        self, workspace: Workspace, context: RunnerContext
+    ) -> RunnerContext:
         raise NotImplementedError()
-    
+
+
 class Engine:
     def __init__(self, server: str, ws: str, runners: list[RunnerBase]) -> None:
         self.__workspace__ = Workspace(server, ws)
         self.__runners__ = runners
         self.__cached__ = list(map(lambda r: str(r.cache), runners))
         self.__cache_status__ = None
-        self.__current_ctx__ = RunnerContext(
-            start=0, tags=[{}]
-        )
+        self.__current_ctx__ = RunnerContext(start=0, tags=[{}])
         while len(self.__cached__) > 0:
             cache_full_path = os.path.join(LME_CACHE_DIR, *self.__cached__, "data")
             if os.path.exists(cache_full_path):
@@ -54,14 +61,17 @@ class Engine:
 
     async def init_resources(self):
         await asyncio.gather(
-            self.__workspace__.create(), *[runner.init_resources() for runner in self.__runners__]
+            self.__workspace__.create(load=None),
+            *[runner.init_resources() for runner in self.__runners__]
         )
 
     async def one_step(self):
         if len(self.__cached__) == len(self.__runners__):
             return False
         runner = self.__runners__[len(self.__cached__)]
-        self.__current_ctx__ = await runner.execute(self.__workspace__, self.__current_ctx__)
+        self.__current_ctx__ = await runner.execute(
+            self.__workspace__, self.__current_ctx__
+        )
         self.__cache_status__ = await self.__workspace__.export()
         self.__cached__.append(str(runner.cache))
         os.mkdir(os.path.join(LME_CACHE_DIR, *self.__cached__))
@@ -69,6 +79,6 @@ class Engine:
         f.write(json.dumps(self.__cache_status__))
         f.close()
         return True
-    
+
     async def clear_resources(self):
         await self.__workspace__.close()
